e0d74ac0d5456d3b19e2b1c91a7c126ef56e16bb
MirrorsChen <1667809009@qq.com>
2018-07-27 17:00:23 +0800

    添加文件

---
+++ testNew.py
@@ -0,0 +1,14 @@
+'''
+@author: mirrorChen
+@license: (C) Copyright 2013-2018, Node Supply Chain Manager Corporation Limited.
+@contact: chenjingxu3@dafycredit.com
+@software: JYB_Android_AT
+@file: testNew.py
+@time: 2018/7/27 16:56
+@desc:
+'''
+
+l1=['李','陈','吴','刘','张']
+l2=['蛋','震男','尊','关张','飞']
+for i,l in zip(l1,l2):
+    print(i,"-->",l)
